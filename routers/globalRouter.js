ad4bc4bc059831e90b5ec8e0374e7dbcd71883b7
SOHEELEE408 <dlwlsrl33@gmail.com>
2020-11-08 21:52:01 +0900

    in connection with Video
    Upload / Detail / Edit / Delete

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -5,17 +5,24 @@ import {
     search
 } from "../controllers/videoController";
 import {
-    join,
-    login,
-    logout
+    logout,
+    getJoin,
+    postJoin,
+    getLogin,
+    postLogin,
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
